ff331ce426cf64c42fbfb031b2a4d9f29e821b66
Unknown <damian.moser2@gbssg.ch>
2018-11-02 19:12:05 +0100

    enhanced gameplay

--- uttt_website/web/main.dart
+++ uttt_website/web/main.dart
@@ -13,53 +13,70 @@ void main() {
   game.start();
 }
 
-void visualize(GameState state) {
-  for (int i = 0; i < 9; i++) {
-    BigTile bigTile = state.tiles[i];
-    String playerCSS;
-    if (bigTile.state == State.p1) playerCSS = "p1BigTile";
-    if (bigTile.state == State.p2) playerCSS = "p2BigTile";
-    if (playerCSS != null) {
-      querySelector(".bigTile$i").classes.add(playerCSS);
-    }
-
-    for (int j = 0; j < 9; j++) {
-      playerCSS = null;
-      Tile tile = bigTile.tiles[j];
-      if (tile.state == State.p1) playerCSS = "p1SmallTile";
-      if (tile.state == State.p2) playerCSS = "p2SmallTile";
-      if (playerCSS != null) {
-        querySelector(".bigTile$i > .tile$j").classes.add(playerCSS);
-      }
-    }
-  }
-}
-
 class WebPlayer implements Player {
   GameState _state;
   GameStateArgument _s;
+  static final List<String> tilePositionHorizontal =  ["top", "top", "top", "middle", "middle", "middle", "bottom", "bottom", "bottom"];
+  static final List<String> tilePositionVertical = ["left", "center", "right", "left", "center", "right", "left", "center", "right"];
 
   WebPlayer() {
     for (int i = 0; i < 9; i++) {
       DivElement bigTile = new DivElement();
+      DivElement tileHolder = new DivElement();
+      bigTile.children.add(tileHolder);
+      tileHolder.classes.add("wrapper");
       bigTile.classes.add("bigTile$i");
       bigTile.classes.add("bigTile");
+      bigTile.classes.add(tilePositionHorizontal[i]);
+      bigTile.classes.add(tilePositionVertical[i]);
       for (int j = 0; j < 9; j++) {
         DivElement tile = new DivElement();
         tile.onClick.listen((event) {
           if (_state != null) {
             Move m = Move.ofIndex(State.p1, i, j);
-            GameController.playMove(_state, m);
-            visualize(_state);
-            _s(_state);
+            if (GameController.getAllPossibleMoves(_state).contains(m)) {
+              GameController.playMove(_state, m);
+              visualize(_state);
+              _s(_state);
+            }
           }
         });
         tile.classes.add("tile$j");
         tile.classes.add("tile");
-        bigTile.children.add(tile);
+        tileHolder.children.add(tile);
+        tile.classes.add(tilePositionHorizontal[j]);
+        tile.classes.add(tilePositionVertical[j]);
       }
       querySelector("#tileWrapper").children.add(bigTile);
     }
+  }
+
+  void visualize(GameState state) {
+    for (int i = 0; i < 9; i++) {
+      BigTile bigTile = state.tiles[i];
+      String playerCSS;
+      if (bigTile.state == State.p1) playerCSS = "p1BigTile";
+      if (bigTile.state == State.p2) playerCSS = "p2BigTile";
+      if (playerCSS != null) {
+        querySelector(".bigTile$i").classes.add(playerCSS);
+      }
+
+      for (int j = 0; j < 9; j++) {
+        playerCSS = null;
+        Tile tile = bigTile.tiles[j];
+        if (tile.state == State.p1) playerCSS = "p1SmallTile";
+        if (tile.state == State.p2) playerCSS = "p2SmallTile";
+        if (playerCSS != null) {
+          querySelector(".bigTile$i > .wrapper > .tile$j").classes.add(playerCSS);
+        }
+      }
+    }
+    querySelectorAll(".yellow").forEach((e) => e.classes.remove("yellow"));
+    GameController.getAllPossibleMoves(state).forEach((m){
+      querySelector(".bigTile${m.bigIndex} > .wrapper > .tile${m.smallIndex}").classes.add("yellow");
+
+    });
+
   }
 
   @override
