fbb9a7a6ac84e0a04f88958aba996e6a32c1f019
Unknown <damian.moser2@gbssg.ch>
2019-01-23 09:41:34 +0100

    Theme stays over sideload

--- uttt_website/web/components/Navbar.dart
+++ uttt_website/web/components/Navbar.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:html';
 
 class Navbar {
@@ -7,30 +8,54 @@ class Navbar {
     if (_navbar == null) {
       _navbar = Navbar._internal();
     }
+    _navbar.loadStandart();
     return _navbar;
   }
 
   Navbar._internal() {
     querySelectorAll(".navControl").forEach((e) {
       e.onClick.listen((event) {
-        loadColorTheme(e.innerHtml);
+        _loadColorTheme(e.innerHtml);
       });
     });
-
-    loadColorTheme("powderblue");
   }
 
   List<String> colorThemes = ["powderblue", "coral", "mint"];
 
-  void loadColorTheme(String innerHtml) {
-      colorThemes.forEach((s) {
-        if (innerHtml.toLowerCase() == s) {
-          (querySelector('#${s}') as LinkElement).disabled = false;
-          print("true: $innerHtml");
-        } else {
-          (querySelector('#${s}') as LinkElement).disabled = true;
-          print("False: $innerHtml");
-        }
-      });
-   }
+  void loadStandart() {
+    Map<String, String> cookies = _getCookies();
+    if (cookies.containsKey("theme")) {
+      _loadColorTheme(cookies["theme"]);
+    } else {
+      _loadColorTheme("powderblue");
+    }
+  }
+
+  Map<String, String> _getCookies() {
+    Map<String, String> cookies = Map();
+    print(document.cookie.split(";").length);
+    document.cookie.split(";").forEach((c) {
+      if (c.split("=").length == 2) cookies[c.split("=")[0]] = c.split("=")[1];
+    });
+    return cookies;
+  }
+
+  void _setCookies(Map<String, String> cookies) {
+    cookies.entries
+        .map((entry) => "${entry.key}=${entry.value}")
+        .forEach((s) => document.cookie = s);
+  }
+
+  void _loadColorTheme(String innerHtml) {
+    colorThemes.forEach((s) {
+      if (innerHtml.toLowerCase() == s) {
+        (querySelector('#${s}') as LinkElement).disabled = false;
+        Map<String, String> cookies = _getCookies();
+        cookies["theme"] = innerHtml;
+        _setCookies(cookies);
+      } else {
+        (querySelector('#${s}') as LinkElement).disabled = true;
+      }
+    });
+  }
 }
