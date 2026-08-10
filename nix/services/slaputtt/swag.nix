1105d37618c7f8ee7e704b4d5b68986c8f91c391
Damian Moser <damian.d.moser@gmail.com>
2026-08-10 20:29:05 +0200

    swag: use listen-https.conf and geo-access for proxy_protocol
    Neo ingress now dual-listens (LAN 443 + PROXY protocol). Vhosts must
    include listen-https.conf and geo-access.conf instead of listen 443 ssl.

--- nix/services/slaputtt/swag.nix
+++ nix/services/slaputtt/swag.nix
@@ -9,11 +9,11 @@
   in {
     config.neo.services.slaputtt.proxyConf = lib.mkDefault ''
       server {
-        listen 443 ssl;
+        include /config/nginx/listen-https.conf;
         http2 on;
         server_name ${cfg.subdomain}.*;
         include /config/nginx/ssl.conf;
-
+        include /config/nginx/geo-access.conf;
         client_max_body_size 0;
 
         location / {
